904b61bb018ae40327fd218f298b0e39cf3dba96
astind <dillon@astin.dev>
2025-11-02 15:51:16 -0500

    add search page

---
+++ src/routes/search.tsx
@@ -0,0 +1,13 @@
+
+export default function SearchPage() {
+  return(
+    <div class="flex flex-col">
+      <div class="flex justify-center">
+        <h1>Search Public Lists</h1>
+      </div>
+      <div class="flex justify-end">
+        <p>Looking for groups? Check out the public groups page</p>
+      </div>
+    </div>
+  )
+}
